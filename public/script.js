a548cd6eebaf4f361f053315379dc4526a37bab7
TristanKatte <tristankatte@hotmail.com>
2024-04-24 10:49:33 +0200

    .

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
 console.log('Script is running');
+const myButton = document.getElementById("myButton");
+
+// Disable the button
+myButton.disabled = true;
+
+// Enable the button
+myButton.disabled = false;
+
+const popup = document.getElementById("popup");
+
+// Show the pop-up message
+popup.classList.remove("hidden");
+
+// Hide the pop-up message after 3 seconds
+setTimeout(() => {
+  popup.classList.add("hidden");
+}, 3000);
