e1a25a2f8d165014c8af7caacaf08fdc45cfff08
TristanKatte <tristankatte@hotmail.com>
2024-04-25 23:44:07 +0200

    dialog toegevoegd voor UI state

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
 // console.log('Script is running');
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+showButton.addEventListener("click", () => {
+    favDialog.showModal();
+  });
+
+  favDialog.addEventListener("close", (e) => {
+    outputBox.value =
+      favDialog.returnValue === "default"
+        ? "No return value."
+        : `ReturnValue: ${favDialog.returnValue}.`;
+
+        confirmBtn.addEventListener("click", (event) => {
+            event.preventDefault(); // We don't want to submit this fake form
+            favDialog.close(selectEl.value); // Have to send the select box value here.
+            });
