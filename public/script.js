83974838384198fac6c3c135015dd594d7739b4e
TristanKatte <tristankatte@hotmail.com>
2024-05-06 00:21:08 +0200

    .

--- public/script.js
+++ public/script.js
@@ -3,10 +3,10 @@ const modal = document.querySelector('#favDialog');
 const openModal = document.querySelector('.open-button');
 const closeModal = document.querySelector('.close-button');
 
-openModal.addEventListener('click', () => {
+openModal.addEventListener('click', function()  {
   modal.showModal();
 })
 
-openModal.addEventListener('click', () => {
-  Modal.close();
+openModal.addEventListener('click', function() {
+  modal.close();
 });
